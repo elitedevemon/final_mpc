967d14ee034f5084df8739b65c8d74ce112ede1e
elitedev_emon <elitedevemon@gmail.com>
2021-12-30 15:08:16 +0600

    Changes into result page

--- resources/views/backend/pages/result.blade.php
+++ resources/views/backend/pages/result.blade.php
@@ -1,15 +1,17 @@
 @extends('layouts.student-master')
 @section('student-title', 'Student Dashboard')
 @section('student-master-styles')
-@include('backend.partials.result.partials.result-styles')
+  @include('backend.partials.result.partials.result-styles')
+  @livewireStyles()
 @endsection
 @section('student-content')
-      <!-- ============================================================== -->
-      <!-- Start right Content here -->
-      <!-- ============================================================== -->
-      @include('backend.partials.result.result')
-      <!-- end main content-->
+  <!-- ============================================================== -->
+  <!-- Start right Content here -->
+  <!-- ============================================================== -->
+  @livewire('backend.result')
+  <!-- end main content-->
 @endsection
 @section('student-master-scripts')
-@include('backend.partials.result.partials.result-scripts')
+  @include('backend.partials.result.partials.result-scripts')
+  @livewireScripts()
 @endsection
---
+++ resources/views/livewire/backend/result.blade.php
@@ -0,0 +1,51 @@
+<div>
+  <div class="app-content main-content">
+    <div class="side-app">
+  
+      <!--Page header-->
+      <div class="page-header">
+        <div class="page-leftheader">
+          <h4 class="page-title mb-0 text-primary">All Results</h4>
+        </div>
+      </div>
+      <!--End Page header-->
+  
+      <div class="card">
+        <div class="card-body">
+          <div class="table-responsive">
+            <table class="table table-bordered text-nowrap" id="example2">
+              <thead>
+                <tr>
+                  <th class="wd-15p border-bottom-0">SI</th>
+                  <th class="wd-15p border-bottom-0">Topics</th>
+                  <th class="wd-20p border-bottom-0">Date</th>
+                  <th class="wd-15p border-bottom-0">Marks</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>01</td>
+                  <td>Preposition</td>
+                  <td>03/02/2021</td>
+                  <td>12</td>
+                </tr>
+                <tr>
+                  <td>02</td>
+                  <td>Modal Verb</td>
+                  <td>04/03/2021</td>
+                  <td>11</td>
+                </tr>
+                <tr>
+                  <td>03</td>
+                  <td>Adverb</td>
+                  <td>05/02/2022</td>
+                  <td>15</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
