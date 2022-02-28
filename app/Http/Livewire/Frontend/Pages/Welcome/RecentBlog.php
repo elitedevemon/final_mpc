7d05892f3dfa5e6cdeec4695de63878ac69c9450
EliteDev Emon <elitedevemon@gmail.com>
2022-02-28 13:06:14 +0600

    1.0.0.5

--- app/Http/Livewire/Frontend/Pages/Welcome/RecentBlog.php
+++ app/Http/Livewire/Frontend/Pages/Welcome/RecentBlog.php
@@ -4,9 +4,12 @@ namespace App\Http\Livewire\Frontend\Pages\Welcome;
 
 use App\Models\frontend\Post;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class RecentBlog extends Component
 {
+  use WithPagination;
+  
   /**
    * Render function
    * @var recentPosts
@@ -14,7 +17,7 @@ class RecentBlog extends Component
   public function render()
   {
     return view('livewire.frontend.pages.welcome.recent-blog', [
-      'recentPosts' => Post::all(),
+      'recentPosts' => Post::orderBy('id', 'DESC')->paginate(3),
     ]);
   }
 }
